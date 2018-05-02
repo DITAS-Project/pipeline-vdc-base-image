eb569d63462bf4588400af56bebe760a6be1d8ce
Sebastian Werner <sebastian.werner@outlook.com>
2018-05-02 15:17:04 +0200

    extended entrypoint.sh for new vdc-agent version

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Starting the monitoring services"
 cd /opt/monitoring
-exec ./vdc-agent -zipkin "http://${zipkinURI}/api/v1/spans" -vdc "http://${vdcURI}" &
+exec ./vdc-agent -zipkin "http://${zipkinURI}/api/v1/spans" -vdc "http://${vdcURI}" -elastic "http://${elasticURI}"&
 exec java -jar VDCMonitor.jar &
 cd /
 
