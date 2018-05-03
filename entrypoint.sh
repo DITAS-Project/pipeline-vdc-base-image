e689cb5cd3d5568b64a8da13e8718877b68d4fda
Sebastian Werner <sebastian.werner@outlook.com>
2018-05-03 15:30:24 +0200

    modified image start sequence

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Starting the monitoring services"
 cd /opt/monitoring
-exec ./vdc-agent -zipkin "http://${zipkinURI}/api/v1/spans" -vdc "http://${vdcURI}" -elastic "http://${elasticURI}"&
+exec ./vdc-agent -zipkin "http://${zipkinURI}/api/v1/spans" -vdc "http://${vdcURI}" -elastic "http://${elasticURI}" &
 exec java -jar VDCMonitor.jar &
 cd /
 
 #start ngnix
-exec sh /run.sh
-
+exec sh /run.sh &
+cd ${WORKINGDIR}
 exec "$@"
