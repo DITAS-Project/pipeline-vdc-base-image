a0abde06219d468733509d90daec246b3778f53b
Sebastian Werner <sebastian.werner@outlook.com>
2018-06-18 11:19:21 +0200

    fixed startup issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 sleep 60
 
-if [[ -z "${ENVCONF}" ]]; then
+if [ -n "$ENVCONF" ]; then
+    echo "rewriting configs"
     envsubst '${vdcURI},${elasticURI},${zipkinURI}' < /.config/monitor.json > /.config/monitor.json 
     envsubst '${vdcURI},${elasticURI},${zipkinURI}' < /.config/traffic.json > /.config/traffic.json 
     envsubst '${vdcURI},${elasticURI},${zipkinURI}' < /.config/logging.json > /.config/logging.json 
