8aac393c270a4dcef0924576b4045594fecfe2ff
Sebastian Werner <sebastian.werner@outlook.com>
2018-09-06 10:37:37 +0200

    added verbose logging to all componentes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
-sleep 60
+
 
 if [ -n "$ENVCONF" ]; then
+    sleep 60
     echo "rewriting configs"
     envsubst '${vdcURI},${elasticURI},${zipkinURI}' < /.config/monitor.json > /.config/monitor.json 
     envsubst '${vdcURI},${elasticURI},${zipkinURI}' < /.config/traffic.json > /.config/traffic.json 
@@ -11,11 +12,11 @@ fi
 
 echo "Starting the monitoring services"
 cd /opt/monitoring
-exec ./vdc-agent &
-exec ./vdc-traffic &
+exec ./vdc-agent --verbose &
+exec ./vdc-traffic --verbose &
 cd /
 
 #start proxy
-exec ./request-monitor  &
+exec ./request-monitor  --verbose &
 cd ${WORKINGDIR}
 exec "$@"
